52c2661f6d00733432b594209a8f073989df92d5
Takenori <takenori113@gmail.com>
2024-04-23 19:34:41 +0900

    すべての問題を終えました。

--- express.js
+++ express.js
@@ -1,3 +1,20 @@
+import express from 'express';
+const app = express();
 const port = 3001;
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
+
+app.get('/hello', (req, res) => {
+  res.json({
+    "message": "hello"
+  });
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
 // expressでWebサーバーを実装
--- http.js
+++ http.js
@@ -1,4 +1,14 @@
 const hostname = "localhost";
 const port = 3000;
+import { createServer } from 'node:http';
+var server = createServer(
+  (request,response)=>{
+    response.setHeader('Content-Type','text/plain');
+    response.end('Hello World')
+  }
+);
 
-// httpモジュールでWebサーバーを実装
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); ;
+
